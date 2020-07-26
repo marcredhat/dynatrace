5d5eae29491f54eb363c9bbd23ab67007087ebe3
Marc Chisinevski <mchisine@redhat.com>
2020-07-26 13:23:12 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+#check JSON models e.g. #https://www.dynatrace.com/support/help/dynatrace-api/configuration-api/service-api/custom-services-api/get-all/?response-parameters%3C-%3Ejson-model=json-model#parameters
+#to see what info is available from Dynatrace
 ubicontainer=$(buildah from registry.access.redhat.com/ubi8/ubi-minimal:latest)
 export CONTAINER_FS=$(buildah mount)
 #buildah run $ubicontainer microdnf update -y && \
@@ -6,4 +8,22 @@ buildah run $ubicontainer git clone https://github.com/Dynatrace/dynatrace-cli.g
 
 buildah run $ubicontainer  pip3 install requests
 buildah config --cmd /bin/bash ${ubicontainer} && \
-buildah run $ubicontainer -- sh -c "python3 dynatrace-cli/dtcli.py --help"
+buildah run $ubicontainer -- sh -c "python3 dynatrace-cli/dtcli.py config apitoken <Dynatrace API token> tenanthost <XXXXXXX.live.dynatrace.com>"
+printf "\n***Display my OpenShift nodes using Dynatrace CLI***\n"
+buildah run $ubicontainer -- sh -c "python3 dynatrace-cli/dtcli.py ent host .*ocp*. displayName"
+printf "\n***Display the IP addresses of my OpenShift nodes using Dynatrace CLI***\n"
+buildah run $ubicontainer -- sh -c "python3 dynatrace-cli/dtcli.py ent host .*ocp*. ipAddresses"
+printf "\n***Display applications using Dynatrace CLI***\n"
+buildah run $ubicontainer -- sh -c "python3 dynatrace-cli/dtcli.py ent app  .*web*. displayName"
+
+#https://www.dynatrace.com/support/help/dynatrace-api/configuration-api/service-api/custom-services-api/get-all/?response-parameters%3C-%3Ejson-model=json-model#parameters
+#buildah run $ubicontainer -- sh -c "python3 dynatrace-cli/dtcli.py ent srv serviceTechnologyTypes=Java  discoveredName"
+#['Internal service', 'Jenkins v2.222.1', 'Jenkins v2.222.1', 'Requests executed in background threads of com.install4j.runtime.launcher.UnixLauncher nexus-*', 'Requests executed in background threads of jenkins.war jenkins-*', 'Requests executed in background threads of org.sonar.server.app.WebServer sonarqube-*', 'ScriptResource', 'Sonatype Nexus']
+#buildah run $ubicontainer -- sh -c "python3 dynatrace-cli/dtcli.py ent srv .*Jenkins*. discoveredName"
+
+#buildah run $ubicontainer -- sh -c "python3 dynatrace-cli/dtcli.py evt query from=60 to=0 host .*ocp.*"
+
+#buildah run $ubicontainer -- sh -c "python3 dynatrace-cli/dtcli.py ent app .*web*."
+#{'nextEventStartTms': None, 'nextEventId': None, 'nextCursor': None, 'from': 1595790690000, 'to': 1595794290000, 'totalEventCount': 0, 'events': []}
+#['APPLICATION-EA7C4B59F27D43EB']
+#buildah run $ubicontainer -- sh -c "python3 dynatrace-cli/dtcli.py  evt query entityId=APPLICATION-EA7C4B59F27D43EB"
