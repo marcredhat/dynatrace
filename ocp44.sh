a32d1dee38f75459e4ad56a9217cf3b4d5366465
Marc Chisinevski <mchisine@redhat.com>
2020-07-30 10:43:40 -0700

    Update ocp44.sh

--- ocp44.sh
+++ ocp44.sh
@@ -1,1 +1,1 @@
-sudo ssh -X root@dell-r730-001  -L 443:console-openshift-console.apps.ocp4.local:443 -L 22:localst:22 -L 80:nexus-cicd-marc.apps.ocp4.local:80 -L 80:ansible-tower-web-svc-tower371.apps.ocp4.local:80 -L 8080:localhost:8080
+sudo ssh -X root@dell-r730-001  -L 443:console-openshift-console.apps.ocp4.local:443 -L 80:nexus-cicd-marc.apps.ocp4.local:80 -L 80:ansible-tower-web-svc-tower371.apps.ocp4.local:80 -L 8080:localhost:8080
