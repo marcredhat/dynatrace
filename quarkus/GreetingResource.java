d371790d0b80d3a04373a6a74ede1344a9873622
Marc Chisinevski <mchisine@redhat.com>
2020-08-01 20:28:11 -0700

    Update GreetingResource.java

--- quarkus/GreetingResource.java
+++ quarkus/GreetingResource.java
@@ -1,3 +1,5 @@
+//https://github.com/Dynatrace/OneAgent-SDK-for-Java/blob/master/samples/webrequest/src/main/java/com/dynatrace/oneagent/sdk/samples/webrequest/FakedWebserver.java
+
 package org.acme.rest;
 
 import javax.ws.rs.GET;
@@ -20,9 +22,22 @@ public class GreetingResource {
 
     public String hello() {
          String url="/";
-         OneAgentSDK oneAgentSDK = OneAgentSDKFactory.createInstance();
-         webAppInfo = oneAgentSDK.createWebApplicationInfo("servername", "BillingService", "/billing");
-         IncomingWebRequestTracer incomingWebrequestTracer = oneAgentSDK.traceIncomingWebRequest(webAppInfo, url, "GET");
-         return "hello";
+	 OneAgentSDK oneAgentSDK = OneAgentSDKFactory.createInstance();
+	 webAppInfo = oneAgentSDK.createWebApplicationInfo("servername", "BillingService", "/billing");
+	 IncomingWebRequestTracer incomingWebrequestTracer = oneAgentSDK.traceIncomingWebRequest(webAppInfo, url, "GET");
+	 incomingWebrequestTracer.start();
+		try {
+			//response.setContent("Hello world!".getBytes());
+			//response.setStatusCode(200);
+			incomingWebrequestTracer.setStatusCode(200);
+		} catch (Exception e) {
+			// we assume, container is sending http 500 in case of exception is thrown while serving an request:
+			incomingWebrequestTracer.error(e);
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		} finally {
+			incomingWebrequestTracer.end();
+		}
+	 return "hello";
     }
 }
