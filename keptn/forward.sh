31e69a8fde5794a0b3c651e3e3e67d4ac857b042
Marc Chisinevski <mchisine@redhat.com>
2020-08-12 00:18:04 -0700

    Create forward.sh

---
+++ keptn/forward.sh
@@ -0,0 +1,7 @@
+#kubectl -n keptn port-forward service/api-gateway-nginx 8080:80
+kubectl -n keptn port-forward service/api-gateway-nginx 8080:80 &
+KEPTN_ENDPOINT=http://localhost:8080/api
+KEPTN_API_TOKEN=$(kubectl get secret keptn-api-token -n keptn -ojsonpath={.data.keptn-api-token} | base64 --decode)
+keptn auth --endpoint=$KEPTN_ENDPOINT --api-token=$KEPTN_API_TOKEN
+kubectl port-forward svc/bridge -n keptn 9000:8080 &
+keptn configure bridge --output
