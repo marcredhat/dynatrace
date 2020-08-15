2f26f62d88bf50d8604c4fd3245c579f6c30777a
Marc Chisinevski <mchisine@redhat.com>
2020-08-14 18:13:29 -0700

    Create onboard_service.sh

---
+++ keptn/onboard_service.sh
@@ -0,0 +1,2 @@
+keptn onboard service carts --project=keptnopenshift45 --chart=./carts
+keptn send event new-artifact --project=keptnopenshift45 --service=carts --image=docker.io/keptnexamples/carts --tag=0.11.1
